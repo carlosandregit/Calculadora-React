fdd54ce75de070cb320c0e4dfc755116ea6f59e4
carlosandregit <66290422+carlosandregit@users.noreply.github.com>
2022-11-02 00:32:19 -0300

    Atualizando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
+import ReactCli from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactCli.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
